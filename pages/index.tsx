212edf60ba4b84342a54adf93809fe848abc5d41
AFURO-1130 <abcshotaro616@gmail.com>
2023-01-12 12:49:21 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 const index = () => {
-  return (
-    <div>index</div>
-  )
+    return (
+        <>
+            <div>index</div>
+            <a type='button' href="/call">通話体験してみる</a>
+        </>
+    )
 }
 
 export default index
